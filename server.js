a13c7e0542b0f6f9b2b494e3d1d81bbff11850eb
pdkokare1 <pdkokare1@gmail.com>
2025-10-21 21:37:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 
 // Security & Performance Middleware
 app.use(helmet());
