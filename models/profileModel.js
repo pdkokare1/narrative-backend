c7400bfcb934cbd3a2b3a7106edace83be9edd4f
pdkokare1 <pdkokare1@gmail.com>
2025-10-28 00:38:45 +0530

    Create profileModel.js

---
+++ models/profileModel.js
@@ -0,0 +1,32 @@
+// In file: models/profileModel.js
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  // This links the profile to the Firebase Auth user
+  userId: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    index: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    trim: true 
+  },
+  // We will use this for User Stats!
+  articlesViewedCount: { 
+    type: Number, 
+    default: 0 
+  }
+}, {
+  timestamps: true // Adds createdAt and updatedAt
+});
+
+module.exports = mongoose.model('Profile', profileSchema);
