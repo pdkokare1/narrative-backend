6d6aa677bdb1c0c1b4612267545a2deaffe9ef52
pdkokare1 <pdkokare1@gmail.com>
2025-10-28 13:04:32 +0530

    Update activityLogModel.js

--- models/activityLogModel.js
+++ models/activityLogModel.js
@@ -2,20 +2,20 @@
 const mongoose = require('mongoose');
 
 const activityLogSchema = new mongoose.Schema({
-  userId: { 
-    type: String, 
-    required: true, 
-    index: true 
+  userId: {
+    type: String,
+    required: true,
+    index: true
   },
   articleId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Article',
+    ref: 'Article', // Links to the Article model
     required: true
   },
   action: {
     type: String,
     required: true,
-    enum: ['view_analysis', 'view_comparison', 'share_article'], // Types of actions we track
+    enum: ['view_analysis', 'view_comparison', 'share_article', 'read_external'], // Added read_external
     default: 'view_analysis'
   }
 }, {
@@ -24,5 +24,8 @@ const activityLogSchema = new mongoose.Schema({
 
 // Compound index for fast user lookups
 activityLogSchema.index({ userId: 1, timestamp: -1 });
+// Index for fetching stats based on action
+activityLogSchema.index({ userId: 1, action: 1, timestamp: -1 });
+
 
 module.exports = mongoose.model('ActivityLog', activityLogSchema);
