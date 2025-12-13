ce3acef3a1eb9605c7c522b3370644cab1155f1d
pdkokare1 <pdkokare1@gmail.com>
2025-12-13 10:36:48 +0530

    Update asyncHandler.ts

--- utils/asyncHandler.ts
+++ utils/asyncHandler.ts
@@ -1,11 +1,10 @@
 // utils/asyncHandler.ts
 import { Request, Response, NextFunction } from 'express';
 
-// Defines a function that takes Express arguments and returns a Promise
 type AsyncFunction = (req: Request, res: Response, next: NextFunction) => Promise<any>;
 
 const asyncHandler = (fn: AsyncFunction) => (req: Request, res: Response, next: NextFunction) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-export default asyncHandler;
+export = asyncHandler;
