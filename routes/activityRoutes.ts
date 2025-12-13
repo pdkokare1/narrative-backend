7a1f2cdd5e1580e8d03975d8838a16f713b4e9e9
pdkokare1 <pdkokare1@gmail.com>
2025-12-13 12:33:39 +0530

    Update and rename activityRoutes.js to activityRoutes.ts

--- routes/activityRoutes.ts
+++ routes/activityRoutes.ts
@@ -1,17 +1,16 @@
-// routes/activityRoutes.js (FINAL v5.1 - Secured)
-const express = require('express');
-const router = express.Router();
-const asyncHandler = require('../utils/asyncHandler');
-const validate = require('../middleware/validate'); // <--- NEW
-const schemas = require('../utils/validationSchemas'); // <--- NEW
+// routes/activityRoutes.ts
+import express, { Request, Response } from 'express';
+import asyncHandler from '../utils/asyncHandler';
+import validate from '../middleware/validate';
+import schemas from '../utils/validationSchemas';
+import ActivityLog from '../models/activityLogModel';
+import Profile from '../models/profileModel';
 
-// Models
-const ActivityLog = require('../models/activityLogModel');
-const Profile = require('../models/profileModel');
+const router = express.Router();
 
 // --- 1. Log View (Analysis) ---
-router.post('/log-view', validate(schemas.logActivity), asyncHandler(async (req, res) => {
-    const { articleId } = req.body; // Validated by Joi
+router.post('/log-view', validate(schemas.logActivity), asyncHandler(async (req: Request, res: Response) => {
+    const { articleId } = req.body; 
     
     await ActivityLog.create({ userId: req.user.uid, articleId, action: 'view_analysis' });
     await Profile.findOneAndUpdate({ userId: req.user.uid }, { $inc: { articlesViewedCount: 1 } });
@@ -20,7 +19,7 @@ router.post('/log-view', validate(schemas.logActivity), asyncHandler(async (req,
 }));
 
 // --- 2. Log Comparison ---
-router.post('/log-compare', validate(schemas.logActivity), asyncHandler(async (req, res) => {
+router.post('/log-compare', validate(schemas.logActivity), asyncHandler(async (req: Request, res: Response) => {
     const { articleId } = req.body;
     await ActivityLog.create({ userId: req.user.uid, articleId, action: 'view_comparison' });
     await Profile.findOneAndUpdate({ userId: req.user.uid }, { $inc: { comparisonsViewedCount: 1 } });
@@ -28,7 +27,7 @@ router.post('/log-compare', validate(schemas.logActivity), asyncHandler(async (r
 }));
 
 // --- 3. Log Share ---
-router.post('/log-share', validate(schemas.logActivity), asyncHandler(async (req, res) => {
+router.post('/log-share', validate(schemas.logActivity), asyncHandler(async (req: Request, res: Response) => {
     const { articleId } = req.body;
     await ActivityLog.create({ userId: req.user.uid, articleId, action: 'share_article' });
     await Profile.findOneAndUpdate({ userId: req.user.uid }, { $inc: { articlesSharedCount: 1 } });
@@ -36,10 +35,10 @@ router.post('/log-share', validate(schemas.logActivity), asyncHandler(async (req
 }));
 
 // --- 4. Log Read (External Link) ---
-router.post('/log-read', validate(schemas.logActivity), asyncHandler(async (req, res) => {
+router.post('/log-read', validate(schemas.logActivity), asyncHandler(async (req: Request, res: Response) => {
     const { articleId } = req.body;
     await ActivityLog.create({ userId: req.user.uid, articleId, action: 'read_external' });
     res.status(200).json({ message: 'Logged read' });
 }));
 
-module.exports = router;
+export default router;
