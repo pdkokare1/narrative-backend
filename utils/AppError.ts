179810e4a68e710b6fd0025cfab3bd678f972a3f
pdkokare1 <pdkokare1@gmail.com>
2025-12-17 17:43:02 +0530

    Create AppError.ts

---
+++ utils/AppError.ts
@@ -0,0 +1,28 @@
+// utils/AppError.ts
+
+/**
+ * Operational Error Class
+ * Use this to throw errors that are expected and should be sent to the client
+ * (e.g., "User not found", "Invalid input", "Permission denied")
+ */
+class AppError extends Error {
+  public statusCode: number;
+  public status: string;
+  public isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+
+    this.statusCode = statusCode;
+    // If code is 4xx, status is 'fail', else 'error'
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    
+    // Operational errors are predictable (user input bad, etc.)
+    // Programming errors (bugs) are not operational.
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default AppError;
