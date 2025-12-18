143d263e1d68da2210d420ac4c7ab17236d9d4cc
pdkokare1 <pdkokare1@gmail.com>
2025-12-18 10:23:42 +0530

    Update express.d.ts

--- types/express.d.ts
+++ types/express.d.ts
@@ -4,7 +4,6 @@ import { DecodedIdToken } from 'firebase-admin/auth';
 declare global {
   namespace Express {
     interface Request {
-      // Improved: Now specifically uses Firebase's official user definition
       user?: DecodedIdToken;
     }
   }
