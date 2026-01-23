d31ff7a8b5c1f40ee02e765e5d6056788eb246e6
pdkokare1 <pdkokare1@gmail.com>
2026-01-23 12:19:48 +0530

    Update express.d.ts

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,10 +1,14 @@
 // types/express.d.ts
 import { DecodedIdToken } from 'firebase-admin/auth';
+import { IUserRole } from './index'; // Adjust path if needed
 
 declare global {
   namespace Express {
     interface Request {
-      user?: DecodedIdToken;
+      user?: DecodedIdToken & {
+        role?: IUserRole; // Custom claim
+        [key: string]: any;
+      };
     }
   }
 }
